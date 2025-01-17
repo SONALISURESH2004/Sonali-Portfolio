a01ed398ec81647751967f097147c54b8f8b8832
Sonali Suresh <sonalisuresh004@gmail.com>
2025-01-17 18:36:52 +0530

    lastly

--- Asserts/JS/script.js
+++ Asserts/JS/script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+const text = document.querySelector(".sec-text");
+        const textLoad = () => {
+            setTimeout(() => {
+                text.textContent = "Front-end  Developer";
+            },0);
+            setTimeout(() => {
+                text.textContent = "Public Speaker";
+            },1000);
+            setTimeout(() => {
+                text.textContent = "Bibliophile";
+            },3000);
+            setTimeout(() => {
+                text.textContent = "Techie";
+            },5000);
+        
+        }
+    
+        textLoad();
+        setInterval(textLoad, 7000);
